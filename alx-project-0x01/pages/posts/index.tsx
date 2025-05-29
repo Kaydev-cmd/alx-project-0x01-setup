f79740f9d98e6ba8880b567d7994a734fe7a4d98
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-29 16:28:52 +0200

    feat: added PostModal for creating new posts with controlled form. Managed modal state and post state ussing useState. Included responsive styling and layout using Tailwind CSS

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostProps, PostData } from "@/interfaces";
 
 interface PostsPageProps {
   posts: PostProps[];
 }
 
 const Posts: React.FC<PostsPageProps> = ({ posts }) => {
-  console.log(posts);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<PostData | null>(null);
+
+  const handleAddPost = (newPost: PostData) => {
+    setPost({ ...newPost, id: posts.length + 1 });
+  };
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
           <h1 className="text-2xl font-semibold">Post Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-md text-white">
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-md text-white"
+          >
             Add Post
           </button>
         </div>
@@ -31,6 +40,12 @@ const Posts: React.FC<PostsPageProps> = ({ posts }) => {
           ))}
         </div>
       </main>
+      {isModalOpen && (
+        <PostModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddPost}
+        />
+      )}
     </div>
   );
 };
