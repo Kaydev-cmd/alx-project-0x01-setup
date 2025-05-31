8193689103be1cba2545dc45563144f8bade04bd
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-31 12:20:56 +0200

    feat: add placeholder for the phone input field

--- alx-project-0x01/components/common/UserModal.tsx
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -140,6 +140,7 @@ const UserModal: React.FC<UserModalProps> = ({ onClose, onSubmit }) => {
               value={post.phone}
               onChange={handleChange}
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your phone number"
             />
           </div>
           <div className="mb-4">
