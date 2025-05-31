1f6297805ada7b6825adc708133f37f566bb586b
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-31 11:40:11 +0200

    feat: initialize post state with default UserData structure in UserModal

---
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { UserData, UserModalProps } from "@/interfaces";
+
+const UserModal: React.FC<UserModalProps> = ({ onClose, onSubmit }) => {
+  const [post, setPost] = useState<UserData>({
+    id: 0,
+    name: "",
+    username: "",
+    email: "",
+    address: {
+      street: "",
+      suite: "",
+      city: "",
+      zipcode: "",
+      geo: {
+        lat: "",
+        lng: "",
+      },
+    },
+    phone: "",
+    website: "",
+    company: {
+      name: "",
+      catchPhrase: "",
+      bs: "",
+    },
+  });
+  return <div>UserModal</div>;
+};
+
+export default UserModal;
