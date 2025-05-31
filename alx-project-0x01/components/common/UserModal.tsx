313aa1501dd4d15b525eb1697bbf6bb82bdeb09a
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-31 12:10:08 +0200

    feat: add input for username with styling and controlled state

--- alx-project-0x01/components/common/UserModal.tsx
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -74,6 +74,22 @@ const UserModal: React.FC<UserModalProps> = ({ onClose, onSubmit }) => {
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
+          <div className="mb-4">
+            <label
+              htmlFor="userName"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              User Name
+            </label>
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              value={post.username}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
         </form>
       </div>
     </div>
