7531d695440514fe2c5392f43e41cc51b0895383
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-31 12:06:27 +0200

    feat: add form input for User ID with styling and controlled state

--- alx-project-0x01/components/common/UserModal.tsx
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -37,7 +37,31 @@ const UserModal: React.FC<UserModalProps> = ({ onClose, onSubmit }) => {
     onClose();
   };
 
-  return <div>UserModal</div>;
+  return (
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center">
+      <div className="bg-white rounded-lg p-8 shadow-lg w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-4  text-gray-800">Add New User</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="userId"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              User ID
+            </label>
+            <input
+              type="number"
+              id="userId"
+              name="userId"
+              value={post.id}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default UserModal;
