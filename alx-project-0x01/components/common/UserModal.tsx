4d51fb015a2e6bd62079558b29ff3c496bc2b14e
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-05-31 12:15:30 +0200

    feat: add input for phone number with styling and controlled state

--- alx-project-0x01/components/common/UserModal.tsx
+++ alx-project-0x01/components/common/UserModal.tsx
@@ -122,6 +122,22 @@ const UserModal: React.FC<UserModalProps> = ({ onClose, onSubmit }) => {
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
+          <div className="mb-4">
+            <label
+              htmlFor="phone"
+              className="block text-gray-700 font-medium mb-2"
+            >
+              Phone
+            </label>
+            <input
+              type="number"
+              id="phone"
+              name="phone"
+              value={post.phone}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
         </form>
       </div>
     </div>
